91692a976f88e5b663da129956359d411ffa9794
Yoann Blein <yoann.blein@free.fr>
2015-07-08 16:51:24 +0200

    settable seed via flags

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"time"
+	"flag"
 
 	"github.com/yblein/tavor-isa/parse"
 
@@ -28,22 +29,27 @@ func printStrategies() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "usage: <ISA configuration file> [fuzzing strategy]")
+	seed := flag.Int64("seed", -1, "seed for randomness")
+	strategyName := flag.String("strategy", defaultStrategyName, "fuzzing strategy")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s <ISA configuration file>\nOptionnal flags:\n", os.Args[0])
+		flag.PrintDefaults()
 		printStrategies()
+	}
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	file := os.Args[1]
+	file := flag.Arg(0)
 	root, err := parse.Parse(file)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
-	}
-
-	strategyName := defaultStrategyName
-	if len(os.Args) >= 3 {
-		strategyName = os.Args[2]
 	}
 
 	//graph.WriteDot(root, os.Stdout)
@@ -60,13 +66,17 @@ func main() {
 		os.Exit(3)
 	}
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	strat, err := strategy.New(strategyName, root)
+	strat, err := strategy.New(*strategyName, root)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		printStrategies()
 		os.Exit(4)
 	}
+
+	if *seed < 0 {
+		*seed = time.Now().UnixNano()
+	}
+	r := rand.New(rand.NewSource(*seed))
 
 	continueFuzzing, err := strat.Fuzz(r)
 	if err != nil {
